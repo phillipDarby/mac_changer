39318b03ee5a702a1517d9dd8b2998572302df7c
Phil <darby.phillip@gmail.com>
2024-02-21 22:41:08 -0500

    added reading mac address

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -24,5 +25,13 @@ def change_mac(interface, new_mac):
 options = get_arguments()
 #change_mac(options.interface, options.new_mac)
 
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
+ifconfig_result = str(subprocess.check_output(["ifconfig", options.interface]))
+#print(ifconfig_result)
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+if mac_address_search_result:
+    print(mac_address_search_result.group(0))
+else:
+    print("[-] Could not read MAC address.")
+
