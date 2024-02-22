0ce686eba9536186ff75201d6d316f9f95b946e5
Phil <darby.phillip@gmail.com>
2024-02-21 22:02:19 -0500

    Added Output of adapter

--- mac_changer.py
+++ mac_changer.py
@@ -22,4 +22,7 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 options = get_arguments()
-change_mac(options.interface, options.new_mac)
+#change_mac(options.interface, options.new_mac)
+
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+print(ifconfig_result)
