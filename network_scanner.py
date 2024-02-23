f6726ed03f5aa5fc17c4d0d990b8784be162663c
Phil <darby.phillip@gmail.com>
2024-02-22 23:55:45 -0500

    Modified and cleaned up code

--- network_scanner.py
+++ network_scanner.py
@@ -3,6 +3,12 @@
 import scapy.all as scap
 
 def scan(ip):
-    scap.arping(ip)
+    arp_request = scap.ARP(pdst=ip)
+    broadcast = scap.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
 
-scan("172.20.69.50")
+    answered_list = scap.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+
+scan("172.20.69.0/24")
